fd7c3adcce479989cbd7d1749a7dcbed7d73ec72
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-01-22 21:50:06 +0545

    add category and listing model

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    summary = models.CharField(max_length=150)
+
+class Listing(models.Model):
+    title = models.CharField(max_length=50)
+    short_description = models.CharField(max_length=200)
+    full_description = models.CharField(max_length=1000)
+
+
