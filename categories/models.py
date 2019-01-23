9d459cda907a73464fb0345f09d347f4775ece75
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-01-23 20:23:12 +0545

    associate Listing model with Category model

--- categories/models.py
+++ categories/models.py
@@ -8,6 +8,7 @@ class Category(models.Model):
     summary = models.CharField(max_length=150)
 
 class Listing(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=50)
     short_description = models.CharField(max_length=200)
     full_description = models.CharField(max_length=1000)
