f3416bc41d84407d8ecb14a6844522d3addd2b8b
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-01-23 20:27:14 +0545

    define human readable version of Category and Listing model

--- categories/models.py
+++ categories/models.py
@@ -6,11 +6,14 @@ class Category(models.Model):
         verbose_name_plural = "categories"
     name = models.CharField(max_length=50)
     summary = models.CharField(max_length=150)
+    def __str__(self):
+        return self.name
 
 class Listing(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=50)
     short_description = models.CharField(max_length=200)
     full_description = models.CharField(max_length=1000)
-
+    def __str__(self):
+        return self.title
 
