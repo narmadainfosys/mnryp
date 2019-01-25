577a9b24aef6ed20f828a565cc78077702e0d425
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-01-25 17:24:44 +0545

    create view for displaying all listings which fall under a particular category

--- categories/views.py
+++ categories/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Listing
 # Create your views here.
 def categories(request):
@@ -8,3 +8,14 @@ def listings(request):
     listings = Listing.objects
     return render(request, 'categories/listings.html', {'listings':listings})
 
+def category(request, slug):
+    template = 'categories/category.html'
+
+    category = get_object_or_404(Category, slug=slug)
+    post = Listing.objects.filter(category = category)
+
+    context = {
+        'category':category,
+        'post':post,
+    }
+    return render(request, template, context)
