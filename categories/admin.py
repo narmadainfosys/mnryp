ad5b19bad6ec77abc6d9c337337acd14f7b2722d
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-01-25 17:11:36 +0545

    set list display on categories admin page with name and slug

--- categories/admin.py
+++ categories/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Category,Listing
 
 class CategoryAdmin(admin.ModelAdmin):
-    #list_display = ('name', 'slug')
+    list_display = ('name', 'slug')
     prepopulated_fields = {'slug': ('name',)}
 
 # Register your models here.
