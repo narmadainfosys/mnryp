6bf04d6805f5ba5aefa4027ddca1bfa6148d0583
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-01-28 10:47:09 +0545

    add django-allauth url

--- mnryp_project/settings.py
+++ mnryp_project/settings.py
@@ -137,3 +137,6 @@ AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
     "allauth.account.auth_backends.AuthenticationBackend",
 )
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = 'home'
