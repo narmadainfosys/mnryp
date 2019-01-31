02752b0c410027be38a2c615ffaa03d7ae87d98c
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-01-31 13:09:00 +0545

    add twitter login/signup

--- mnryp_project/settings.py
+++ mnryp_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.github',
+    'allauth.socialaccount.providers.twitter',
 ]
 
 MIDDLEWARE = [
