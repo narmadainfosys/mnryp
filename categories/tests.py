d8d5b8bdb55b2c431185d9afe534b1f723b7fbd6
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-04-01 21:49:17 +0545

    write unit test for categories creation

--- categories/tests.py
+++ categories/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from datetime import datetime
+from categories.models import Category
 # Create your tests here.
+
+class CategoriesTest(TestCase):
+    def create_category(self, name='Test category 1',summary='This is test category 1'):
+        return Category.objects.create(name=name,summary=summary)
+
+    def test_category_creation(self):
+        c = self.create_category()
+
+        self.assertEqual(c.name,'Test category 1')
+        self.assertEqual(c.summary, 'This is test category 1')
+        self.assertEqual(c.slug, 'test-category-1')
+        self.assertEqual(c.__str__(), c.name)
+        self.assertTrue(isinstance(c, Category))
