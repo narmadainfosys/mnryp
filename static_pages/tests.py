470ade394eb3774e8ea15fce56dbb974c077efd9
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-04-08 14:42:02 +0545

    write unit test for static_pages to assert template used

--- static_pages/tests.py
+++ static_pages/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
-
+from django.urls import reverse
 # Create your tests here.
+
+class StaticPagesTest(TestCase):
+
+    def test_template_used(self):
+        url_about = reverse('about')
+        url_home = reverse('home')
+        url_contact = reverse('contact')
+        url_privacy = reverse('privacy_policy')
+
+        resp_about = self.client.get(url_about)
+        resp_home = self.client.get(url_home)
+        resp_contact = self.client.get(url_contact)
+        resp_privacy = self.client.get(url_privacy)
+
+        self.assertTemplateUsed(resp_about, 'static_pages/about.html')
+        self.assertTemplateUsed(resp_home, 'static_pages/home.html')
+        self.assertTemplateUsed(resp_contact, 'static_pages/contact.html')
+        self.assertTemplateUsed(resp_privacy, 'static_pages/privacy_policy.html')
