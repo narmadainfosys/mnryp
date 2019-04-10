b60dfa10179a6503df6aebc406554536dda0c4fa
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-04-10 14:16:31 +0545

    add static files directory to the settings

--- mnryp_project/settings.py
+++ mnryp_project/settings.py
@@ -133,7 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+]
 # Database stuff for heroku
 import dj_database_url
 db_from_env = dj_database_url.config()
