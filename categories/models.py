c03fa7cff3c1a8175ee0bfd595fa096b522b66b1
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-09-04 10:24:36 +0545

    adjust tests according to new changes: make phone_numbers and business_fax fields char field

--- categories/models.py
+++ categories/models.py
@@ -24,9 +24,9 @@ class Listing(models.Model):
     contact_person = models.CharField(max_length=50)
     business_address = models.CharField(max_length=50)
     email_address = models.EmailField(blank=True)
-    business_fax = models.PositiveIntegerField(blank=True)
-    phone_number = models.PositiveIntegerField()
-    phone_number_second = models.PositiveIntegerField(blank=True)
+    business_fax = models.CharField(blank=True, max_length=10)
+    phone_number = models.CharField(max_length=10)
+    phone_number_second = models.CharField(blank=True, max_length=10)
     zip_code = models.PositiveIntegerField(blank=True)
     slug = AutoSlugField(max_length=150, unique=True, populate_from='title')
 
